e9b1c7cdbc48d0cfbf95b34f2e80522eb6aea5ec
Marcel Feliu Saperas <marcel.feliu@gracia.lasalle.cat>
2026-04-29 20:18:29 +0200

    feature: add missing setters and functions to viewModel

--- app/src/main/java/com/example/android_studio_test_exercice/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/example/android_studio_test_exercice/viewmodel/MainViewModel.kt
@@ -1,36 +1,50 @@
 package com.example.android_studio_test_exercice.viewmodel
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
+
 import androidx.compose.ui.state.ToggleableState
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class MainViewModel: ViewModel {
+class MainViewModel : ViewModel {
     // Atributs
     private val _estatSwitch: MutableLiveData<Boolean>
-    public val estatSwitch: LiveData<Boolean>
+    val estatSwitch: LiveData<Boolean>
 
     private val _esVegetaria: MutableLiveData<Boolean>
-    public val esVegetaria: LiveData<Boolean>
+    val esVegetaria: LiveData<Boolean>
 
     private val _esVega: MutableLiveData<Boolean>
-    public val esVega: LiveData<Boolean>
+    val esVega: LiveData<Boolean>
 
     private val _esCarnivor: MutableLiveData<Boolean>
-    public val esCarnivor: LiveData<Boolean>
+    val esCarnivor: LiveData<Boolean>
 
     private val _triStateStatus: MutableLiveData<ToggleableState>
-    public val triStateStatus: LiveData<ToggleableState>
+    val triStateStatus: LiveData<ToggleableState>
 
     private val _selectedOption: MutableLiveData<String>
-    public val selectedOption: LiveData<String>
+    val selectedOption: LiveData<String>
+
+    private val _sliderValue: MutableLiveData<Float>
+    val sliderValue: LiveData<Float>
+
+    private val _expanded: MutableLiveData<Boolean>
+    val expanded: LiveData<Boolean>
+
+    private val _selectedItem: MutableLiveData<String>
+    val selectedItem: LiveData<String>
+
+    private val _searchText: MutableLiveData<String>
+    val searchText: LiveData<String>
+
+    private val _showSnackbar: MutableLiveData<Boolean>
+    val showSnackbar: LiveData<Boolean>
+
+    private val _toggleState: MutableLiveData<Boolean>
+    val toggleState: LiveData<Boolean>
 
     /**
-     * Constructor de la classe HelloViewModel
-     * que inicialitzen els atributs
+     * Constructor que inicialitza tots els atributs
      */
     constructor() : super() {
         this._estatSwitch = MutableLiveData<Boolean>(true)
@@ -50,26 +64,81 @@ class MainViewModel: ViewModel {
 
         this._selectedOption = MutableLiveData<String>("Messi")
         this.selectedOption = this._selectedOption
+
+        this._sliderValue = MutableLiveData<Float>(0f)
+        this.sliderValue = this._sliderValue
+
+        this._expanded = MutableLiveData<Boolean>(false)
+        this.expanded = this._expanded
+
+        this._selectedItem = MutableLiveData<String>("Opció A")
+        this.selectedItem = this._selectedItem
+
+        this._searchText = MutableLiveData<String>("")
+        this.searchText = this._searchText
+
+        this._showSnackbar = MutableLiveData<Boolean>(false)
+        this.showSnackbar = this._showSnackbar
+
+        this._toggleState = MutableLiveData<Boolean>(false)
+        this.toggleState = this._toggleState
     }
 
-    fun toggleEstatSwitch(){
-        this._estatSwitch.value = !(this._estatSwitch.value)!!
+    // setters
+    fun toggleEstatSwitch() {
+        _estatSwitch.value = !(_estatSwitch.value ?: true)
     }
 
-    fun toggleEsCarnivor(){
-        this._esCarnivor.value = !(this._esCarnivor.value)!!
+    fun toggleEsCarnivor() {
+        _esCarnivor.value = !(_esCarnivor.value ?: true)
     }
 
-    fun toggleTriStateStatus(){
-        when(this._triStateStatus.value){
+    fun toggleEsVegetaria() {
+        _esVegetaria.value = !(_esVegetaria.value ?: false)
+    }
+
+    fun toggleEsVega() {
+        _esVega.value = !(_esVega.value ?: false)
+    }
+
+    fun setSelectedOption(player: String) {
+        _selectedOption.value = player
+    }
+
+    fun setSliderValue(value: Float) {
+        _sliderValue.value = value
+    }
+
+    fun setExpanded(isExpanded: Boolean) {
+        _expanded.value = isExpanded
+    }
+
+    fun setSelectedItem(item: String) {
+        _selectedItem.value = item
+    }
+
+    fun setSearchText(text: String) {
+        _searchText.value = text
+    }
+
+    fun performSearch() {
+        _showSnackbar.value = true
+    }
+
+    fun toggle() {
+        _toggleState.value = !(_toggleState.value ?: false)
+    }
+
+    fun toggleTriStateStatus() {
+        when (_triStateStatus.value) {
             ToggleableState.On -> setTriStateStatus(ToggleableState.Off)
             ToggleableState.Off -> setTriStateStatus(ToggleableState.Indeterminate)
             ToggleableState.Indeterminate -> setTriStateStatus(ToggleableState.On)
-            null -> setTriStateStatus(ToggleableState.On)
+            else -> setTriStateStatus(ToggleableState.On)
         }
     }
 
-    private fun setTriStateStatus(triState: ToggleableState){
-        this._triStateStatus.value = triState
+    private fun setTriStateStatus(triState: ToggleableState) {
+        _triStateStatus.value = triState
     }
 }
